52ac1c49af666fc1d1fd5a7477363e0e347f18fc
Abderrazzak Benbouya <benbouya.abderrazzak@gmail.com>
2023-02-22 20:57:23 +0100

    feat(alef): set the member id sequence to start from 101

--- global.d.ts
+++ global.d.ts
@@ -1,13 +1,13 @@
 import { Entity } from '@platformatic/sql-mapper';
 import graphqlPlugin from '@platformatic/sql-graphql'
+import { Member } from './types/Member'
 import { Meter } from './types/Meter'
 import { Reading } from './types/Reading'
-import { Member } from './types/Member'
 
 declare module '@platformatic/sql-mapper' {
   interface Entities {
+    member: Entity<Member>,
     meter: Entity<Meter>,
     reading: Entity<Reading>,
-    member: Entity<Member>,
   }
 }
--- migrations/001.do.sql
+++ migrations/001.do.sql
@@ -1,5 +1,13 @@
+-- create a sequence for the members table that start from 100
+CREATE SEQUENCE IF NOT EXISTS members_id_seq
+    START WITH 101
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
 CREATE TABLE IF NOT EXISTS members (
-    id SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY DEFAULT nextval('members_id_seq'),
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     father_name TEXT NOT NULL,
