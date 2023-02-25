b44722f80a0ccaff1f55987fc964e67249a91b56
Abderrazzak Benbouya <benbouya.abderrazzak@gmail.com>
2023-02-25 01:05:16 +0100

    feat(Alef): create a meter id sequence that start from 101

--- migrations/002.do.sql
+++ migrations/002.do.sql
@@ -1,6 +1,14 @@
+-- Create meters squences
+CREATE SEQUENCE IF NOT EXISTS meters_id_seq
+    START WITH 101
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
 -- CREATE METERS TABLE
 CREATE TABLE IF NOT EXISTS meters (
-    id SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY DEFAULT nextval('meters_id_seq'),
     serial_id TEXT NOT NULL,
     label TEXT NOT NULL,
     connected_address TEXT NOT NULL,
