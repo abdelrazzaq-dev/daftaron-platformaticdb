d1467415d956b5c37395de453e2ad74b3d888569
Abderrazzak Benbouya <benbouya.abderrazzak@gmail.com>
2023-02-26 23:16:08 +0100

    chore(alef): re-run migrations

--- global.d.ts
+++ global.d.ts
@@ -3,8 +3,8 @@ import graphqlPlugin from '@platformatic/sql-graphql'
 import { Member } from './types/Member'
 import { Meter } from './types/Meter'
 import { Reading } from './types/Reading'
-import { Invoice } from './types/Invoice'
 import { Rate } from './types/Rate'
+import { Invoice } from './types/Invoice'
 import { Payment } from './types/Payment'
 
 declare module '@platformatic/sql-mapper' {
@@ -12,8 +12,8 @@ declare module '@platformatic/sql-mapper' {
     member: Entity<Member>,
     meter: Entity<Meter>,
     reading: Entity<Reading>,
-    invoice: Entity<Invoice>,
     rate: Entity<Rate>,
+    invoice: Entity<Invoice>,
     payment: Entity<Payment>,
   }
 }
