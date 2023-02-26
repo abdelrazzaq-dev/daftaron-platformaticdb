1b3d6a8393a0036fa789d562ad93d895acfe6b2d
Abderrazzak Benbouya <benbouya.abderrazzak@gmail.com>
2023-02-26 23:16:41 +0100

    feat(alef): add an api route for getting monthly consumption data

--- plugin.js
+++ plugin.js
@@ -2,4 +2,25 @@
 'use strict'
 
 /** @param {import('fastify').FastifyInstance} app */
-module.exports = async function (app) {}
+module.exports = async function (app) {
+    const {db, sql} = app.platformatic
+    async function getMonthlyWaterConsumption() {
+        const results = await db.query(sql`
+            SELECT
+                DATE_TRUNC('month', reading_date) AS month,
+                SUM(reading_value) AS total_consumption
+            FROM
+                readings
+            GROUP BY
+                month
+            ORDER BY
+                month ASC;
+        `)
+        
+        return results
+    }
+
+    app.get('/monthly-water-consumption', async function (request, reply) {
+        return {data: await getMonthlyWaterConsumption()}
+    })
+}
