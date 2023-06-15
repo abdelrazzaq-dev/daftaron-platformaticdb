b6e08b413c6d1b0b989fb6bccc92bc68815dc2bc
Abderrazzak Benbouya <benbouya.abderrazzak@gmail.com>
2023-06-15 23:03:39 +0200

    alef: add payment migration

---
+++ migrations/007.do.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS payments (
+    id SERIAL PRIMARY KEY,
+    invoice_id INTEGER NOT NULL REFERENCES invoices(id),
+    payment_date DATE NOT NULL,
+    payment_hash UUID UNIQUE NOT NULL,
+    payment_amount INTEGER NOT NULL,
+    payment_method TEXT NOT NULL,
+    payment_status TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
