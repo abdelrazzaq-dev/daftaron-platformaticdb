858f6872529c2f69fd7e16c5c54d4a919f0ae4cc
Abderrazzak Benbouya <benbouya.abderrazzak@gmail.com>
2023-06-27 16:53:26 +0200

    alef: update meter_no characters length

--- migrations/002.do.sql
+++ migrations/002.do.sql
@@ -12,7 +12,7 @@ CREATE SEQUENCE IF NOT EXISTS meter_id_seq START WITH 101;
 CREATE TABLE meters (
     id INTEGER PRIMARY KEY DEFAULT NEXTVAL('meter_id_seq'),
     member_id INTEGER NOT NULL REFERENCES members(id),
-    meter_no VARCHAR(20) NOT NULL UNIQUE,
+    meter_no VARCHAR(50) NOT NULL UNIQUE,
     meter_setting_id INTEGER NOT NULL REFERENCES meter_settings(id),
     meter_status VARCHAR(20) NOT NULL,
     meter_address VARCHAR(200) NOT NULL,
